6de98e1454dfc7230331d838190d72cbe68913ef
James Frank <james.frank@publicissapient.com>
2023-05-09 13:30:14 -0400

    (#84) Add window variable for build
    Gatsby needs a fake window variable available during build even if this isn't used at build time.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,27 +5,33 @@ import { Footer } from "./Footer";
 import '../scss/styles.scss';
 
 export default function Layout({ children }) {
-		return (
-				<div role={"document"} className={"pcp-document-container"}>
-						<Header/>
-						<div className={"pcp-grid-section"}>
-								<div className={"desktop-constrained"}>
-										<div className={"grid-row"}>
-												<div className={"grid-col-12"}>
-														<div className={"pcp-sections"}>
-																{ children }
-														</div>
-												</div>
-										</div>
-								</div>
+	return (
+		<div role={"document"} className={"pcp-document-container"}>
+			<Header />
+			<div className={"pcp-grid-section"}>
+				<div className={"desktop-constrained"}>
+					<div className={"grid-row"}>
+						<div className={"grid-col-12"}>
+							<div className={"pcp-sections"}>
+								{children}
+							</div>
 						</div>
-						<Footer/>
-						<Script type="text/javascript">{(function() {
-							if (window._satellite) {
-								_satellite.pageBottom();
-							}
-							})()}
-						</Script>
+					</div>
 				</div>
-		)
+			</div>
+			<Footer />
+			<Script type="text/javascript">{(function () {
+
+				// Make gatsby build happy when window doesn't exist
+				if (typeof window === 'undefined') {
+					global.window = {}
+				}
+
+				if (window._satellite) {
+					_satellite.pageBottom();
+				}
+			})()}
+			</Script>
+		</div>
+	)
 }
