6074bf26ef1d363c83fd6c2e209bdc36b0392486
James Frank <james.frank@publicissapient.com>
2023-05-09 10:46:17 -0400

    (#7) Add Adobe Analytics
    Adds Adobe analytics
    Adds title tags and metadata

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -5,7 +5,7 @@ export const SEO = ({ title, description, pathname, children }) => {
   const { title: defaultTitle, description: defaultDescription, siteUrl } = useSiteMetadata()
 
   const seo = {
-    title: title || defaultTitle,
+    title: title != "" ? title + " - " + defaultTitle : defaultTitle,
     description: description || defaultDescription,
     url: `${siteUrl}${pathname || ``}`,
   }
@@ -14,6 +14,7 @@ export const SEO = ({ title, description, pathname, children }) => {
     <>
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
+      <script src="https://assets.adobedtm.com/6a4249cd0a2c/785de09de161/launch-70d67a6a40a8.min.js"></script>
       {children}
     </>
   )
