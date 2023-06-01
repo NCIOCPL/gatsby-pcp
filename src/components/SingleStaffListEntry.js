633c5db60b8c415d5c440d9bd38a571d3730c9c2
erikscottmeyer <eriksmeyer@gmail.com>
2023-06-01 14:30:10 -0400

    (#123) Removes empty links. Changes nav padding.

--- src/components/SingleStaffListEntry.js
+++ src/components/SingleStaffListEntry.js
@@ -5,40 +5,40 @@ import '../scss/components/_staff.scss';
 import PropTypes from 'prop-types';
 
 class SingleStaffListEntry extends React.Component {
-	displayUrl;
-	linkClass;
-	linkText;
-
 	render() {
 		if (this.props.url && this.props.url.length > 0) {
-			this.displayUrl = this.props.url;
-			this.linkClass = '';
-			this.linkText = 'View Bio';
-		} else {
-			this.displayUrl = '';
-			this.linkClass = 'no-link-display';
-			this.linkText = '';
-		}
-
-		return (
-			<li className={'member'}>
-				<div className={'member-image'}>
-					<Link to={this.displayUrl} className={this.linkClass}>
-						<Image src={this.props.source} alt={this.props.alt} />
-					</Link>
-				</div>
-				<div className={'member-information'}>
-					<div className={'member-name'}>
-						<Link to={this.displayUrl} className={this.linkClass}>
-							{this.props.alt}
+			return (
+				<li className={'member'}>
+					<div className={'member-image'}>
+						<Link to={this.props.url}>
+							<Image src={this.props.source} alt={this.props.alt} />
 						</Link>
 					</div>
-					<div className={'member-title'}>{this.props.title}</div>
-					<div className={'member-number'}>{this.props.number}</div>
-					<div className={'member-email'}>{this.props.email}</div>
-				</div>
-			</li>
-		);
+					<div className={'member-information'}>
+						<div className={'member-name'}>
+							<Link to={this.props.url}>{this.props.alt}</Link>
+						</div>
+						<div className={'member-title'}>{this.props.title}</div>
+						<div className={'member-number'}>{this.props.number}</div>
+						<div className={'member-email'}>{this.props.email}</div>
+					</div>
+				</li>
+			);
+		} else {
+			return (
+				<li className={'member'}>
+					<div className={'member-image'}>
+						<Image src={this.props.source} alt={this.props.alt} />
+					</div>
+					<div className={'member-information'}>
+						<div className={'member-name'}>{this.props.alt}</div>
+						<div className={'member-title'}>{this.props.title}</div>
+						<div className={'member-number'}>{this.props.number}</div>
+						<div className={'member-email'}>{this.props.email}</div>
+					</div>
+				</li>
+			);
+		}
 	}
 }
 
