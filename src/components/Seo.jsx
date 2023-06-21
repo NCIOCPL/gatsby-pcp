d41c4e2e9223d05030b52dbfce53db616d7460ea
James Frank <james.frank@nih.gov>
2023-06-21 15:04:27 -0400

    (#152) Use canonical URLs plugin
    Closes #152

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { useSiteMetadata } from '../hooks/use-site-metadata';
 import PropTypes from 'prop-types';
-import { useLocation } from '@reach/router';
 
 export const SEO = ({ title, description, pathname, children }) => {
 	const { title: defaultTitle, description: defaultDescription, siteUrl } = useSiteMetadata();
-	const location = useLocation();
 
 	const seo = {
 		title: title !== undefined ? title + ' - ' + defaultTitle : defaultTitle,
@@ -16,7 +14,6 @@ export const SEO = ({ title, description, pathname, children }) => {
 	return (
 		<>
 			<title>{seo.title}</title>
-			<link rel="canonical" href={location.href} />
 			<meta name="description" content={seo.description} />
 			<script src="https://assets.adobedtm.com/6a4249cd0a2c/785de09de161/launch-70d67a6a40a8.min.js"></script>
 			{children}
