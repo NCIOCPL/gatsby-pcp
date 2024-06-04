e7077e1b805d2a5c8488248d525ded2441464e24
erikscottmeyer <eriksmeyer@gmail.com>
2024-06-04 15:30:33 -0400

    (#283) New Jaffee blog
    Closes #283

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => {
 				<div className="featured-homepage-box">
 					<SingleHomepageBox url="/ncp" source="ncp-401x357.png" alt="" title="National Cancer Plan" description="The President’s Cancer Panel is charged with monitoring progress toward the goals of the National Cancer Plan. Learn more about the Panel’s role and discover how you can get involved."></SingleHomepageBox>
 					<SingleHomepageBox url="/ncp/reports/" source="Reports_Feature_Card_Thumbnail_401x358.jpg" alt="" title="New Report from the President’s Cancer Panel" description="This report features findings from the Panel's initial assessment of the National Cancer Plan and highlights recommendations in five priority areas to accelerate progress toward the Plan’s goals."></SingleHomepageBox>
-					<SingleHomepageBox url="/about/blogs/pcp-report-national-cancer-plan/" source="ncp-report-cover-card.png" alt="" title="President’s Cancer Panel Issues Report on National Cancer Plan Progress" description="On February 28, 2024, the Panel published its inaugural report to the President on the National Cancer Plan. Read our latest blog post to learn more about the Panel’s assessment process and the five priority areas identified in the report."></SingleHomepageBox>
+					<SingleHomepageBox url="/about/blogs/dr-jaffee-on-career-cancer-research/" source="Dr.-Elizabeth-Jaffee-004-card.jpg" alt="" title='"We Had to Do Better": Dr. Elizabeth Jaffee on a Career in Cancer Research and Treatment' description="This post is the first in a series on getting to know the President’s Cancer Panel. Learn more about the career journey of our Panel Chair, Dr. Elizabeth Jaffee, and how she got involved in pancreatic cancer care and research."></SingleHomepageBox>
 				</div>
 			</div>
 		</Layout>
